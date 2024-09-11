5297b361737bdbe11837fa5bcf0ceaa7dddfeacf
Arthur <anon121213@mail.ru>
2024-09-12 02:03:06 +0300

    final build

--- Bot/index.js
+++ Bot/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "7540708072:AAEArLeZEv8JHLjPWLI990tJFytaH9Gw5CQ";
+const TOKEN = "PASTE_YOUR_TOKEN";
 const server = express();
 const bot = new TelegramBot(TOKEN, {
     polling: true
