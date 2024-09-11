cbfac3bb22e69f00e9da40e3005903aa3001d278
Arthur <anon121213@mail.ru>
2024-09-12 01:44:58 +0300

    final

--- Bot/index.js
+++ Bot/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "7540708072:AAEArLeZEv8JHLjPWLI990tJFytaH9Gw5CQ";
+const TOKEN = "PASTE_YOUR_TELEGRAM_TOKEN";
 const server = express();
 const bot = new TelegramBot(TOKEN, {
     polling: true
@@ -20,7 +20,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "http://localhost";
+        let gameurl = "PASTE_YOUR_GAME_LINK";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
